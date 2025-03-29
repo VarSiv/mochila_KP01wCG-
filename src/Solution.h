a512bfec82523d5efc9ad6adf2f82fe84130e181
VarSiv <var.mironov@gmail.com>
2025-03-29 18:30:22 -0300

    started solution

--- src/Solution.h
+++ src/Solution.h
@@ -3,19 +3,25 @@
 
 #include <vector>
 #include <iostream>
+#include "KP01withCGInstance.h"
 
 class Solution {
 private:
-    // Definir estructura interna.
+    int _num_items;//toda la mochila (intance) [1,2,3,4,5..]
+    vector<int> _items_vector;//solution subconjunto de instance [1,3,4]
+    KP01withCGInstance _mochila;
+    
+
 
 public:
     Solution(int numItems); // Constructor to initialize size
     void addItem(int item);
     void removeItem(int item);
     bool contains(int item) const;
+    int totalProfit();
     void printSolution() const;
     // Pueden agregar los metodos que consideren necesarios.
 };
 
-#endif // SOLUTION_H
+#endif // SOLUTION_Hl
 
